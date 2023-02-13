7ddf5f49cd34d1065ee41d18d44f0e087c088cc8
loyal_goose <polina.alina.2015@mail.ru>
2023-02-13 22:25:08 +0700

    добавить пагинацию с изменением вида при нажатии

--- src/script.js
+++ src/script.js
@@ -1,17 +1,22 @@
-const ul = document.getElementById("users__list");
+const usersList = document.getElementById("users__list");
 const url = "https://jsonplaceholder.typicode.com/users";
 
+let pagination = document.querySelector("#pagination");
+let active,
+  paginationItems = [],
+  users = [],
+  notesOnPage = 3,
+  countOfItems = 0;
+
 //по url делаем запрос, приводим ответ к формату json, затем ответ обрабатывается функцией renderItems
-fetch(url)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    renderItems(data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+
+const fetchData = async () => {
+  const promise = fetch(url);
+  const data = await promise;
+  renderListItems(await data.json());
+};
+
+fetchData();
 
 //функция рендера компонентов списка с данными с сервера:
 // - создаем тэг элемента списка
@@ -19,19 +24,45 @@ fetch(url)
 // - говорим, что родительским элементом li является элемент с id == users__list, т.е. наш тэг ul
 // - вызываем ыункцию, добавляющую всплывающее окно с информацией о пользователе
 // - последнее действие это добавление функции, которая обрабатывает события при перемещении элемента, на вход подаём родительский элемент
-const renderItems = (data) => {
+const renderListItems = (data) => {
   data.map((user) => {
     let li = document.createElement("li");
-
     li.className = "users__item";
     li.draggable = true;
     li.innerText = user.name;
 
-    ul.appendChild(li);
+    usersList.appendChild(li);
 
     addPopupInfo(user, li);
-    addDragEventListeners(ul);
+    addDragEventListeners(usersList);
   });
+
+  countOfItems = Math.ceil(data.length / notesOnPage);
+
+  renderPaginationItems(countOfItems);
+};
+
+const renderPaginationItems = (countOfItems) => {
+  for (let i = 1; i <= countOfItems; i++) {
+    let li = document.createElement("li");
+    li.innerHTML = i;
+    pagination.appendChild(li);
+    paginationItems.push(li);
+  }
+
+  paginationItems.map((item) =>
+    item.addEventListener("click", () => {
+      showPage(item);
+    })
+  );
+};
+
+const showPage = (item) => {
+  active && active.classList.remove("active");
+
+  active = item;
+
+  item.classList.add("active");
 };
 
 const addDragEventListeners = (listElement) => {
@@ -75,6 +106,7 @@ const addDragEventListeners = (listElement) => {
     listElement.insertBefore(activeElement, nextElement);
   });
 };
+
 //на добавление всплывающего окна использовала не атрибут innerText, а innerHTML, так как посчитала, что в данном случае будет удобней.
 // КАк и в случае li, создаем тэг span, которому присваиваем имя класса, и внутрь него помещаем html с данными пользователя
 // Также добавляем события на перемещение мыши: при нахождении курсора на элементе списка показываем доп. информацию, при нахождении вне элемента - скрываем информацию
